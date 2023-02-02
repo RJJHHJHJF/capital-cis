7c7bbc67c904c85700f74f1ebf771218e397fd25
RJJHHJHJF <119957408+RJJHHJHJF@users.noreply.github.com>
2023-02-02 12:49:20 +0530

    change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new code change on repo chabge");
+        System.out.println( "new code change on repo change");
     }
 }
