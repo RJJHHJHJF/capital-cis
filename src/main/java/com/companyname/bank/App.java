ab3b2b3ac9285877c66de59b55e4b06617a5055f
RJJHHJHJF <119957408+RJJHHJHJF@users.noreply.github.com>
2023-04-27 20:04:05 +0530

    poll scm changes

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new CI-github-maven change");
+        System.out.println( "new poll-scm change");
     }
 }
